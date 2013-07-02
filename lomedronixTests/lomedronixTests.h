cf546bfb51816da210022a34d02d77393f63493f
lomelyo <lomelyo@lomeMBP13.local>
2013-07-02 10:44:47 -0500

    initial commit

---
+++ lomedronixTests/lomedronixTests.h
@@ -0,0 +1,13 @@
+//
+//  lomedronixTests.h
+//  lomedronixTests
+//
+//  Created by lomelyo on 7/2/13.
+//  Copyright (c) 2013 OnixMedia. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface lomedronixTests : SenTestCase
+
+@end
---
+++ lomedronixTests/lomedronixTests.m
@@ -0,0 +1,32 @@
+//
+//  lomedronixTests.m
+//  lomedronixTests
+//
+//  Created by lomelyo on 7/2/13.
+//  Copyright (c) 2013 OnixMedia. All rights reserved.
+//
+
+#import "lomedronixTests.h"
+
+@implementation lomedronixTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in lomedronixTests");
+}
+
+@end
